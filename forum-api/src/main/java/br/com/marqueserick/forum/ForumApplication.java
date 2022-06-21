fef7fb0405679c2406fc95e8e66084d0e84c2b29
marqueserick <erickmarques43@gmail.com>
2022-06-21 11:32:02 -0300

    finaliza curso profiles, testes e deploy

--- forum-api/src/main/java/br/com/marqueserick/forum/ForumApplication.java
+++ forum-api/src/main/java/br/com/marqueserick/forum/ForumApplication.java
@@ -2,6 +2,8 @@ package br.com.marqueserick.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -10,7 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSpringDataWebSupport
 @EnableCaching
 @EnableSwagger2
-public class ForumApplication {
+public class ForumApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForumApplication.class, args);
